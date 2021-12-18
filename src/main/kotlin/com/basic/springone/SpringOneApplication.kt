ec3609e45e4ede9b6142f32c5ededac54f7f05de
Bruno Valle <brunolv6@gmail.com>
2021-12-18 14:37:30 -0300

    (setup)

---
+++ src/main/kotlin/com/basic/springone/SpringOneApplication.kt
@@ -0,0 +1,11 @@
+package com.basic.springone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringOneApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringOneApplication>(*args)
+}
---
+++ src/test/kotlin/com/basic/springone/SpringOneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.basic.springone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringOneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
